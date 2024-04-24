e991ebd73c3e44e0f97c7fb0b2c6f43beeed233a
Roman Kosov <11503318+91k@users.noreply.github.com>
2024-04-24 18:30:41 +0300

    build: настраивает хэширование

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
 const path = require('path');
 const CopyFile = require('copy-webpack-plugin');
+const MakeHtml = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
         clean: true,
     },
     devtool: 'source-map',
     plugins: [
+        new MakeHtml({
+            template: 'public/index.html',
+        }),
         new CopyFile({
-            patterns: [{ from: 'public' }],
+            patterns: [{
+                from: 'public',
+                globOptions: { ignore: ['**/index.html'] }
+            }],
         })
     ],
     module: {
