d06322c64c0f933491402d713439fcd153a38027
Roman Kosov <11503318+91k@users.noreply.github.com>
2024-04-24 18:16:03 +0300

    build: настраивает транспайлер

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,20 @@ module.exports = {
             patterns: [{ from: 'public' }],
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
 };
